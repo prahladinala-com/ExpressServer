85f1b3ae9cdd00e983ab049d4e926a4a54ef7e7a
Prahlad Inala <46848783+prprojects@users.noreply.github.com>
2021-01-26 15:11:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+
+const app = express()
+
+const port = 8000
+
+// app.get('/', mymethod)
+// or
+// just use a call back (like below)
+
+app.get('/', (req, res) => {
+  return res.send('You are visiting Home page route...')
+})
+
+app.get('/login', (req, res) => {
+  return res.send('You are visiting login route...')
+})
+
+app.get('/logout', (req, res) => {
+  return res.send('You are logged out route...')
+})
+
+app.get('/signout', (req, res) => {
+  return res.send('You are signed out')
+})
+
+app.get('/hitesh', (req, res) => {
+  return res.send('Hitesh uses Instagram')
+})
+app.listen(port, () => {
+  console.log('Server is up and running....')
+})
+
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
